6f814821bdd9e2e38a4cdbbe83fb0b5807fb6eb3
roberto <roberto@h3b.com.br>
2017-10-12 02:05:06 -0300

    Thursday, 12 of October, 2017

--- Files.py
+++ Files.py
@@ -31,6 +31,7 @@ print(type(f))
 
 """
 
+"""
 ## Binary Bytes Files
 
 import struct
@@ -50,4 +51,52 @@ print(data[4:8])                                # Slice bytes in the middle
 print(list(data))                               # A sequence of 8-bit bytes
 
 print(struct.unpack('>i4sh', data))             # Unpack into objects again
+"""
 
+## Unicode Text Files
+
+S = 'sp\xc4m'                                   # Non-ASCII Unicode text
+print(S)
+print(S[2])                                     # Sequence of characters
+
+file = open('unidata.txt', 'w', encoding='utf-8') # Write/encode UTF-8 text
+
+print(file.write(S))                            # 4 characters written
+
+file.close()
+
+text = open('unidata.txt', encoding='utf-8').read() # Read/decode UTF-8 text
+
+print(text)                                     # Print 4 chars (code points)
+
+print(len(text))
+
+raw = open('unidata.txt', 'rb').read()          # Read raw encoded bytes
+
+print(raw)                                      # Print the raw data
+
+print(len(raw))                                 # It's really 5 bytes in UTF-8 (4 code points)
+
+print(text.encode('utf-8'))                     # Manual encode to bytes
+
+print(raw.decode('utf-8'))                      # Manual decode to str
+
+print(text.encode('latin-1'))                   # Bytes differ in others
+print(text.encode('utf-16'))
+
+print(len(text.encode('latin-1')), len(text.encode('utf-16')))
+
+print(b'\xff\xfes\x00p\x00\xc4\x00m\x00'.decode('utf-16'))  # But same string decoded
+
+""" 
+## Python 2.X
+
+import codecs
+
+X = codecs.open('unidata.txt', encoding='utf-8').read()
+print(X)
+
+open('unidata.txt', 'rb').read()
+
+open('unidata.txt').read()
+"""
